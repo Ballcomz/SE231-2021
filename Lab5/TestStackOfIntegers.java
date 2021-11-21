32584753e7e8bd2839ddaa8306449a74b568c8b4
ballcomz <ballcomz@gmail.com>
2021-11-22 00:23:56 +0700

    Lab4.2

---
+++ Lab5/TestStackOfIntegers.java
@@ -0,0 +1,13 @@
+package Lab5;
+
+public class TestStackOfIntegers {
+    public static void main(String[] args) {
+        StackOfIntegers stack = new StackOfIntegers();
+        
+        for (int i = 0; i < 10; i++)
+          stack.push(i); // Push i to the stack
+        
+        while (!stack.empty()) // Test if stack is empty
+          System.out.print(stack.pop() + " "); // Remove and return from stack
+      }
+}
