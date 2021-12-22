9765e2bce329c6d848aa05ceda2e3d43c05a7dbc
ballcomz <ballcomz@gmail.com>
2021-12-22 14:06:45 +0700

    lab12

---
+++ Lab12/Student.java
@@ -0,0 +1,20 @@
+package Lab12;
+
+public class Student extends Person {
+    public static int FRESHMAN = 1;
+    public static int SOPHOMORE = 2;
+    public static int JUNIOR = 3;
+    public static int SENIOR = 4;
+  
+    protected int status;
+  
+    Student(String name) {
+      super(name);
+    }
+    
+    @Override
+    public String toString() {
+      return "Student: " + getName();
+    }
+  
+}
