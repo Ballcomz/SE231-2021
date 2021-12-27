b611feaa5d7134e05399360a5aead09e4cf21547
ballcomz <ballcomz@gmail.com>
2021-12-27 11:15:41 +0700

    lab13

---
+++ Lab13/Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+    private String name;
+    private String position;
+    private double salary;
+    
+    public Employee(){
+        
+    }
+    
+    public Employee(String name, String position, double salary){
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+    
+    public void setName(String n){
+        this.name = n;
+    }
+    
+    public void setPosition(String p){
+        this.position = p;
+    }
+    
+    public void setSalary(double s){
+        this.salary = s;
+    }
+    
+    public String toString(){
+        return "This empolyee " + this.name + ". Posision is " + this.position + " has salary " + this.salary;
+    }
+
+}
---
+++ Lab13/TestEmployee.java
@@ -0,0 +1,10 @@
+public class TestEmployee {
+    public static void main(String[] args) {
+        Specialist s1 = new Specialist("Kittitouch Suteeca","House keeper",50000.0,"clean floor");
+        Specialist s2 = new Specialist("Parinya Suwansrikham", "Teacher", 20000.0, "Ph.D.");
+        
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
+    }
+
+}
